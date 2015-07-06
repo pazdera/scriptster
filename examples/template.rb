9444c064eaf6889d580b958bf7295235bc7c4b21
Radek Pazdera <radek@kano.me>
2015-07-06 23:28:46 +0100

    Updating the script template

--- examples/template.rb
+++ examples/template.rb
@@ -7,6 +7,8 @@
 require 'docopt'
 require 'scriptster'
 
+include Scriptster
+
 doc = <<DOCOPT
 Usage:
   #{File.basename __FILE__} [-h]
@@ -15,21 +17,21 @@ Options:
   -h, --help          Show this message.
 DOCOPT
 
-begin
-  args = Docopt::docopt doc
-rescue Docopt::Exit => e
-  $stderr.puts e.message
-  exit 1
-end
-
 Scriptster::configure do |conf|
   conf.verbosity = :verbose
   conf.log_format = '%{timestamp} [%{name}] %{type} %{message}'
 end
 
+begin
+  args = Docopt::docopt doc
+rescue Docopt::Exit => e
+  log :err, e.message
+  exit 1
+end
+
 ### >>> Example
-Scriptster::log :info, "Started!"
-ls = Scriptster::cmd 'ls -l | grep -v "^total"',
+log :info, "Listing files:"
+ls = cmd 'ls -l | grep -v "^total"',
   show_out: true,
   show_err: true,
   tag: 'ls -l'
@@ -39,5 +41,5 @@ ls.out.lines.each do |line|
   files.push line.split[-1]
 end
 
-Scriptster::log :info, files.join(', ')
+log :info, files.join(', ')
 # <<<
